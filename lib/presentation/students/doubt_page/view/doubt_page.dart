d5f14a2dddf005a40c7f656a6137975b2a5eb5fe
najeebnaju512 <najeebnaju512@gmail.com>
2024-05-09 10:48:47 +0530

    added student module

--- lib/presentation/students/doubt_page/view/doubt_page.dart
+++ lib/presentation/students/doubt_page/view/doubt_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../core/constants/color_constants.dart';
+import '../../../../core/constants/color_constants.dart';
 
 class DoubtPage extends StatelessWidget {
   const DoubtPage({super.key});
