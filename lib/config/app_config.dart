f054795f2fda447c5770ce5574b79087be0fdab0
najeebnaju512 <najeebnaju512@gmail.com>
2024-05-09 10:35:59 +0530

    basse url

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   //baseurl is set here
-  static String baseurl = "http://10.11.0.179:8000/";
+  static String baseurl = "http://10.11.1.31:8000/";
 
   //key to store data from api function in LoginController in json format used in AppUtils to get accesskey&userid
   static const String loginData = 'logInData';
